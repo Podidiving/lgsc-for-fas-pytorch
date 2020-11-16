535175d25b3eacb5490a383fcb9e6c18993b76c6
Aleks <veselev.aleks@gmail.com>
2020-11-16 13:24:58 +0300

    changed triplet loss

--- src/loss.py
+++ src/loss.py
@@ -1,101 +1,152 @@
+# https://github.com/alfonmedela/triplet-loss-pytorch
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
-# source code from: https://github.com/Yuol96/pytorch-triplet-loss
-def pairwise_distances(embeddings, squared=False):
+def pairwise_distance_torch(embeddings, device):
+    """Computes the pairwise distance matrix with numerical stability.
+    output[i, j] = || feature[i, :] - feature[j, :] ||_2
+    Args:
+      embeddings: 2-D Tensor of size [number of data, feature dimension].
+    Returns:
+      pairwise_distances: 2-D Tensor of size [number of data, number of data].
     """
-    ||a-b||^2 = |a|^2 - 2*<a,b> + |b|^2
-    """
-    # get dot product (batch_size, batch_size)
-    dot_product = embeddings.mm(embeddings.t())
 
-    # a vector
-    square_sum = dot_product.diag()
+    # pairwise distance matrix with precise embeddings
+    precise_embeddings = embeddings.to(dtype=torch.float32)
 
-    distances = (
-        square_sum.unsqueeze(1) - 2 * dot_product + square_sum.unsqueeze(0)
+    c1 = torch.pow(precise_embeddings, 2).sum(axis=-1)
+    c2 = torch.pow(precise_embeddings.transpose(0, 1), 2).sum(axis=0)
+    c3 = precise_embeddings @ precise_embeddings.transpose(0, 1)
+
+    c1 = c1.reshape((c1.shape[0], 1))
+    c2 = c2.reshape((1, c2.shape[0]))
+    c12 = c1 + c2
+    pairwise_distances_squared = c12 - 2.0 * c3
+
+    # Deal with numerical inaccuracies. Set small negatives to zero.
+    pairwise_distances_squared = torch.max(
+        pairwise_distances_squared, torch.tensor([0.0]).to(device)
+    )
+    # Get the mask where the zero distances are at.
+    error_mask = pairwise_distances_squared.clone()
+    error_mask[error_mask > 0.0] = 1.0
+    error_mask[error_mask <= 0.0] = 0.0
+
+    pairwise_distances = torch.mul(pairwise_distances_squared, error_mask)
+
+    # Explicitly set diagonals to zero.
+    mask_offdiagonals = torch.ones(
+        (pairwise_distances.shape[0], pairwise_distances.shape[1])
+    ) - torch.diag(torch.ones(pairwise_distances.shape[0]))
+    pairwise_distances = torch.mul(
+        pairwise_distances.to(device), mask_offdiagonals.to(device)
+    )
+    return pairwise_distances
+
+
+def TripletSemiHardLoss(y_true, y_pred, device, margin=1.0):
+    """Computes the triplet loss_functions with semi-hard negative mining.
+       The loss_functions encourages the positive distances (between a pair of embeddings
+       with the same labels) to be smaller than the minimum negative distance
+       among which are at least greater than the positive distance plus the
+       margin constant (called semi-hard negative) in the mini-batch.
+       If no such negative exists, uses the largest negative distance instead.
+       See: https://arxiv.org/abs/1503.03832.
+       We expect labels `y_true` to be provided as 1-D integer `Tensor` with shape
+       [batch_size] of multi-class integer labels. And embeddings `y_pred` must be
+       2-D float `Tensor` of l2 normalized embedding vectors.
+       Args:
+         margin: Float, margin term in the loss_functions definition. Default value is 1.0.
+         name: Optional name for the op.
+       """
+
+    labels, embeddings = y_true, y_pred
+
+    # Reshape label tensor to [batch_size, 1].
+    lshape = labels.shape
+    labels = torch.reshape(labels, [lshape[0], 1])
+
+    pdist_matrix = pairwise_distance_torch(embeddings, device)
+
+    # Build pairwise binary adjacency matrix.
+    adjacency = torch.eq(labels, labels.transpose(0, 1))
+    # Invert so we can select negatives only.
+    adjacency_not = adjacency.logical_not()
+
+    batch_size = labels.shape[0]
+
+    # Compute the mask.
+    pdist_matrix_tile = pdist_matrix.repeat(batch_size, 1)
+    adjacency_not_tile = adjacency_not.repeat(batch_size, 1)
+
+    transpose_reshape = pdist_matrix.transpose(0, 1).reshape(-1, 1)
+    greater = pdist_matrix_tile > transpose_reshape
+
+    mask = adjacency_not_tile & greater
+
+    # final mask
+    mask_step = mask.to(dtype=torch.float32)
+    mask_step = mask_step.sum(axis=1)
+    mask_step = mask_step > 0.0
+    mask_final = mask_step.reshape(batch_size, batch_size)
+    mask_final = mask_final.transpose(0, 1)
+
+    adjacency_not = adjacency_not.to(dtype=torch.float32)
+    mask = mask.to(dtype=torch.float32)
+
+    # negatives_outside: smallest D_an where D_an > D_ap.
+    axis_maximums = torch.max(pdist_matrix_tile, dim=1, keepdim=True)
+    masked_minimums = (
+        torch.min(
+            torch.mul(pdist_matrix_tile - axis_maximums[0], mask),
+            dim=1,
+            keepdim=True,
+        )[0]
+        + axis_maximums[0]
+    )
+    negatives_outside = masked_minimums.reshape([batch_size, batch_size])
+    negatives_outside = negatives_outside.transpose(0, 1)
+
+    # negatives_inside: largest D_an.
+    axis_minimums = torch.min(pdist_matrix, dim=1, keepdim=True)
+    masked_maximums = (
+        torch.max(
+            torch.mul(pdist_matrix - axis_minimums[0], adjacency_not),
+            dim=1,
+            keepdim=True,
+        )[0]
+        + axis_minimums[0]
+    )
+    negatives_inside = masked_maximums.repeat(1, batch_size)
+
+    semi_hard_negatives = torch.where(
+        mask_final, negatives_outside, negatives_inside
     )
 
-    distances = distances.clamp(min=0)
+    loss_mat = margin + pdist_matrix - semi_hard_negatives
 
-    if not squared:
-        epsilon = 1e-16
-        mask = torch.eq(distances, 0).float()
-        distances += mask * epsilon
-        distances = torch.sqrt(distances)
-        distances *= 1 - mask
+    mask_positives = adjacency.to(dtype=torch.float32) - torch.diag(
+        torch.ones(batch_size)
+    ).to(device)
+    num_positives = mask_positives.sum()
 
-    return distances
-
-
-# source code from: https://github.com/Yuol96/pytorch-triplet-loss
-def get_valid_triplets_mask(labels):
-    """
-    To be valid, a triplet (a,p,n) has to satisfy:
-        - a,p,n are distinct embeddings
-        - a and p have the same label, while a and n have different label
-    """
-    indices_equal = torch.eye(labels.size(0)).byte().to(labels.device)
-    indices_not_equal = ~indices_equal
-    i_ne_j = indices_not_equal.unsqueeze(2)
-    i_ne_k = indices_not_equal.unsqueeze(1)
-    j_ne_k = indices_not_equal.unsqueeze(0)
-    distinct_indices = i_ne_j & i_ne_k & j_ne_k
-
-    label_equal = torch.eq(labels.unsqueeze(1), labels.unsqueeze(0))
-    i_eq_j = label_equal.unsqueeze(2)
-    i_eq_k = label_equal.unsqueeze(1)
-    i_ne_k = ~i_eq_k
-    valid_labels = i_eq_j & i_ne_k
-
-    mask = distinct_indices & valid_labels
-    return mask
-
-
-# source code from: https://github.com/Yuol96/pytorch-triplet-loss
-def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
-    """
-    get triplet loss for all valid triplets and average over those triplets whose loss is positive.
-    """
-
-    distances = pairwise_distances(embeddings, squared=squared)
-
-    anchor_positive_dist = distances.unsqueeze(2)
-    anchor_negative_dist = distances.unsqueeze(1)
-    triplet_loss = anchor_positive_dist - anchor_negative_dist + margin
-
-    # get a 3D mask to filter out invalid triplets
-    mask = get_valid_triplets_mask(labels)
-
-    triplet_loss = triplet_loss * mask.float()
-    triplet_loss.clamp_(min=0)
-
-    # count the number of positive triplets
-    epsilon = 1e-16
-    num_positive_triplets = (triplet_loss > 0).float().sum()
-    num_valid_triplets = mask.float().sum()
-    fraction_positive_triplets = num_positive_triplets / (
-        num_valid_triplets + epsilon
-    )
-
-    triplet_loss = triplet_loss.sum() / (num_positive_triplets + epsilon)
-
-    return triplet_loss, fraction_positive_triplets
+    triplet_loss = (
+        torch.max(
+            torch.mul(loss_mat, mask_positives), torch.tensor([0.0]).to(device)
+        )
+    ).sum() / num_positives
+    triplet_loss = triplet_loss.to(dtype=embeddings.dtype)
+    return triplet_loss
 
 
 class TripletLoss(nn.Module):
-    """Triplet Loss
-
-    arXiv: https://arxiv.org/pdf/1703.07737.pdf
-    """
-
     def __init__(self, margin=0.5):
-        super(TripletLoss, self).__init__()
+        super().__init__()
         self.margin = margin
 
-    def forward(self, pred, target):
-        loss, _ = batch_all_triplet_loss(
-            target, pred, self.margin, squared=True
+    def forward(self, input, target, **kwargs):
+        return TripletSemiHardLoss(
+            target, F.normalize(input, p=2, dim=-1), target.device, self.margin
         )
-        return loss
